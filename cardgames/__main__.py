d74058c9364f8d10ea6c760a26009452a0c8c719
Thomas-Fulton <thomas@fultondesigns.co.uk>
2021-12-14 19:01:29 +0000

    Add arguments

--- cardgames/__main__.py
+++ cardgames/__main__.py
@@ -1,4 +1,22 @@
-from cardgames import menu
+import games
+import argparse
+
+parser = argparse.ArgumentParser(prog="simple_predict")
+parser.add_argument('--game', '-g', type=str),
+parser.add_argument('--loadout', '-l', type=str),
+parser.add_argument("nplayers", type=int)
+args = parser.parse_args()
+
+def main() :
+    def __init__(self, game, loadout, nplayers) :
+        if game == "snap" :
+            games.snap_game()
+        elif game == None :
+            main.options()
+
+    def options(self) :
+        print("Options... \n Use \`--help\` to display options")
+
 
 if __name__ == '__main__':
-    menu.run()
+    main(**vars(args))
