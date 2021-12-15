358c1f5992f96b6f98402e159f90c9f3b28338a2
Thomas-Fulton <thomas@fultondesigns.co.uk>
2021-12-15 16:17:46 +0000

    add argument parsing and draft main()

--- cardgames/__main__.py
+++ cardgames/__main__.py
@@ -1,22 +1,43 @@
-import games
+from .games import snap_game
 import argparse
 
-parser = argparse.ArgumentParser(prog="simple_predict")
-parser.add_argument('--game', '-g', type=str),
-parser.add_argument('--loadout', '-l', type=str),
-parser.add_argument("nplayers", type=int)
-args = parser.parse_args()
 
-def main() :
-    def __init__(self, game, loadout, nplayers) :
-        if game == "snap" :
-            games.snap_game()
-        elif game == None :
-            main.options()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--game', '-g', type=str, choices=['snap', 'speed'])
+    parser.add_argument('--loadout', '-l', type=str, default='new game')
+    parser.add_argument("--player", '-p', type=str, dest='players', action='append', help='Player name.')  # append each occurence of player to list of players
+    parser.add_argument('--instructions', '-i', type=str, default=False)
+    args = parser.parse_args()
+    if args.instructions is True and args.game is None:
+        parser.error('<game> required with --instructions: please specify for which game you would like\
+         instruction.')
+    for arg in vars(args):
+        print("{} is {}".format(arg, getattr(args, arg)))
+    return args
 
-    def options(self) :
-        print("Options... \n Use \`--help\` to display options")
+
+def main():
+    try:
+        args = parse_args()
+    except Exception as e:
+        print("Arguments failed to parse: {}".format(e))
+        raise
+
+    try:
+        print("launching {}".format(args.game))
+        if args.game == "snap":
+            try:
+                snap_game.SnapGame(args.loadout, args.players, args.instructions)
+            except Exception as e:
+                print("Game {} failed: {}".format(args.game, e))
+    except Exception as e:
+        print(e)
+
+#    def options(self) :
+#        print("Options... \n Use \`--help\` to display options")
 
 
 if __name__ == '__main__':
-    main(**vars(args))
+    import sys
+    sys.exit(main())
