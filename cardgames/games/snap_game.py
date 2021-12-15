613625133807f375a44c4e5262360755ccb8691c
Thomas-Fulton <thomas@fultondesigns.co.uk>
2021-12-15 16:59:42 +0000

    fix class and arguments accepted

--- cardgames/games/snap_game.py
+++ cardgames/games/snap_game.py
@@ -1,10 +1,20 @@
-from ..components import game, deck
+#!/usr/bin/env python
+
+from components import game, deck
 
 
-class SnapGame(game.Game()):
+class SnapGame(game.Game):
     def __init__(self, loadout, players, instructions):
+        self.loadout = loadout
+        self.players = players
+        self.instructions = instructions
         nplayers = len(players)
-        print("Let's play Snap! You are playing with {} players: {} \n Let's deal!".format(nplayers, players[1:]))
-        cards = deck.Deck()
-        cards.shuffle()
-        cards.deal(players, "all")
+        print("Let's play Snap! You are playing with {} players: {}".format(nplayers, players[1:]))
+
+    def setup_game(self, loadout, players, instructions):
+        print("Starts the game. Let's deal!")
+        # for player in players:
+        #    player=deck.player
+        # cards = deck.Deck()
+        # cards.shuffle()
+        # cards.deal(self.players, "all")
