5bb1c82443e3d1cd281c6f7c1814478cab334a6e
Thomas-Fulton <thomas@fultondesigns.co.uk>
2022-01-03 13:02:47 +0000

    preliminary setup, deal, shuffle+ functions

--- cardgames/components/deck.py
+++ cardgames/components/deck.py
@@ -1,8 +1,29 @@
+#!/usr/bin/env python
+import random
 
-def Deck() :
-    def __init__(self) :
-        self.cards = list()  # store cards, which ones, how many, what order
-    def shuffle(self) :
-        print("(tmp) deals deck") # random shuffle
-    def deal(self, ncards, piles) :  # needs to be able to take arguments with name of pile.
-        print("(tmp) shuffles deck")
+
+class Deck:
+    def __init__(self):
+        self.card_values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        self.card_suits = ["diamonds", "hearts", "clubs", "spades"]
+        self.all_cards = [(value, suit) for value in self.card_values for suit in self.card_suits]
+
+    def shuffle(self):
+        random.shuffle(self.all_cards)  # random shuffle
+
+    def deal(self, ncards, destinations, location):
+        '''
+        Takes cards from the top of deck, and deals to either the top of bottom of the deck/s listed in destinations
+        '''
+        for pile in destinations:
+            for n in range(ncards):
+                if location == "top":
+                    pile.insert(0, self.all_cards.pop(0))
+                elif location == "bottom":
+                    pile.append(self.all_cards.pop(0))
+
+    def upturn(self, ncards):
+        print(self.all_cards[0:ncards])
+
+    def empty(self):
+        self.all_cards.clear()
