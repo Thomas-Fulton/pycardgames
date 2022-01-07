c6232c1cdee632a2bb640d376caa25feaf381130
Thomas-Fulton <thomas@fultondesigns.co.uk>
2022-01-07 12:28:01 +0000

    pycharm .idea/ folder

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python-card-games',
+    version='1.0',
+    packages=['cardgames', 'cardgames.games', 'cardgames.components'],
+    url='',
+    license='',
+    author='thomas',
+    author_email='thomas@fultondesigns.co.uk',
+    description=''
+)
