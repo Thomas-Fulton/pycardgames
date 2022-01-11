145986ee196980f4e5e5074db4b020b8e57507c0
Thomas-Fulton <thomas@fultondesigns.co.uk>
2022-01-11 13:27:04 +0000

    catch and pass when dealing from empty deck, improve upturn

--- cardgames/components/deck.py
+++ cardgames/components/deck.py
@@ -7,8 +7,8 @@ class Deck:
     Simulates a deck of cards using lists.
     """
     def __init__(self):
-        self.card_values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-        self.card_suits = ["diamonds", "hearts", "clubs", "spades"]
+        self.card_values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+        self.card_suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
         self.all_cards = [(value, suit) for value in self.card_values for suit in self.card_suits]
 
     def shuffle(self):
@@ -19,7 +19,7 @@ class Deck:
 
     def deal(self, ncards, destinations, location):
         """
-        Takes cards from the top of deck, and deals to either the top of bottom of the deck/s listed in destinations
+        Takes cards from the top of deck, and deals to either the top or bottom of the deck/s listed in destinations
 
         :param ncards: number of cards to deal
         :type ncards: int
@@ -30,20 +30,27 @@ class Deck:
         """
         for n in range(ncards):
             for pile in destinations:
-                if location == "top":
-                    pile.insert(0, self.all_cards.pop(0))
-                elif location == "bottom":
-                    pile.append(self.all_cards.pop(0))
+                try:
+                    if location == "top":
+                        pile.insert(0, self.all_cards.pop(0))
+                    elif location == "bottom":
+                        pile.append(self.all_cards.pop(0))
+                except Exception as e:
+                    print(e, pile)
+                    pass
+
+
 
     def upturn(self, ncards):
         """
-        :param ncards: The number of cards to turn over.
+        :param ncards: The number of cards to turn over and display.
         :type ncards: int
         """
-        print(self.all_cards[0:ncards])
+        for k in range(ncards):
+            print("{} of {}".format(self.all_cards[k][0], self.all_cards[k][1]))
 
     def empty(self):
         """
-        Clears all cards from :py:attr:`deck.all_cards`.
+        Clears all cards from :py:attr:`deck.all_cards` to create an empty deck.
         """
         self.all_cards.clear()
