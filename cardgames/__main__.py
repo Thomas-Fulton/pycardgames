47f275fd142fa079470d0eee256fa523ec824e78
Thomas-Fulton <thomas@fultondesigns.co.uk>
2022-01-19 05:14:28 +0000

    improve argparse help, formatting of printed messages

--- cardgames/__main__.py
+++ cardgames/__main__.py
@@ -11,43 +11,53 @@ def parse_args():
     :rtype: :py:class:`argparse.Namespace`
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--game', '-g', type=str, choices=['snap', 'poker'])
-    parser.add_argument('--loadout', '-l', type=str, default='new game')
-    parser.add_argument('--player', '-p', type=str, dest='players', action='append', help='Player name.')  # append each occurence of player to list of players
-    parser.add_argument('--instructions', '-i', action='store_true', help='Display instructions for a specified game')
+    parser.add_argument('-g', '--game', type=str, choices=['snap', 'poker'], help='Specify a game, either to start '
+                                                                                  'up and play, or to read the game\'s '
+                                                                                  'instructions')
+    parser.add_argument('-l', '--loadout', type=str, default='new game', help='Resume a saved game (Not currently '
+                                                                              'available)')
+    parser.add_argument('-p', '--player', type=str, dest='players', action='append', help='Add a player. (At least '
+                                                                                          'two players with unique '
+                                                                                          'names are required for a '
+                                                                                          'game.)')  # action
+    # ='append' adds each occurrence of player to list of players
+    parser.add_argument('-i', '--instructions', action='store_true', help='Print instructions for a specified '
+                                                                          '<game>.')
     args = parser.parse_args()
     if args.instructions is True and args.game is None:
         parser.error('<game> required with --instructions: please specify for which game you would like\
          to display instructions.')
-    for arg in vars(args):
-        print("{} is {}".format(arg, getattr(args, arg)))
+    # for arg in vars(args):
+    #    print("{} is {}".format(arg, getattr(args, arg)))
     return args
 
 
 def main():
-    """
-    Launches game module using parsed command line arguments.
+    """Launches game module using parsed command line arguments.
     """
     args = parse_args()
+    print("\n\n\n        Welcome to Python cardgames!\n\n\n")
+
     try:
         if args.game == "snap":
-            print("Launching {}\n\n\n".format(args.game))
+            print("Launching \'{}\'...\n\n\n".format(args.game))
             this_game = snap_game.SnapGame(args.loadout, args.players, args.instructions)
 
         elif args.game == "poker":
-            print("Launching {}...\n\n\n".format(args.game))
+            print("Launching \'{}\'...\n\n\n".format(args.game))
             this_game = poker_game.PokerGame(args.loadout, args.players, args.instructions)
 
-    except Exception as e:
-        print("Game {} failed: {}".format(args.game, e))
-        raise
+        else:
+            print("(Use `--help` for further options)\n")
 
-#    def options(self) :
-#        print("Options... \n Use \`--help\` to display options")
+    except Exception as e:
+        print("Game {} failed to launch: \n{}".format(args.game, e))
+        raise
 
 
 if __name__ == '__main__':
     import sys
+
     sys.exit(main())
 
 # TODO bot automation
