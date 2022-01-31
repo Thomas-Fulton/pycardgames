3f7ba852970e7c277cb84f68d5a48df1749f7860
Thomas-Fulton <thomas@fultondesigns.co.uk>
2022-01-31 16:53:03 +0000

    testing setup

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 setup(
     name='python-card-games',
     version='1.0',
-    packages=['cardgames', 'cardgames.games', 'cardgames.components'],
-    url='',
+    packages=['cardgames', 'cardgames.games', 'cardgames.components'], # list packages (folders) not files
     license='',
     author='thomas',
     author_email='thomas@fultondesigns.co.uk',
-    description=''
+    description='',
+    long_description = open('README.txt').read(),
+    url='https://github.com/Thomas-Fulton/python-card-games.git',
 )
