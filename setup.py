74ce716b3b5c5243b4620cdfcbfc1dcc3c4f356c
Thomas-Fulton <thomas@fultondesigns.co.uk>
2022-02-01 15:28:48 +0000

    update urls, python_requires, author

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     version='0.1.0',
     packages=['cardgames', 'cardgames.games', 'cardgames.components'], # list packages (folders) not files
     license='MIT',
-    author='thomas',
+    author='T. Fulton',
     author_email='thomas@fultondesigns.co.uk',
     description='',
     long_description=open('README.md').read(),
-    url='https://github.com/Thomas-Fulton/python-card-games.git',
+    project_urls={'Source': 'https://github.com/Thomas-Fulton/python-card-games', 'Documentation': 'tbd'},
+    python_requires='>=3',
     entry_points={'console_scripts': [
         'pycardgames = cardgames.__main__:main'], }
 )
