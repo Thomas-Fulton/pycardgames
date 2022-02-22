89cdcd7e0a560fabe0bc97f2702a5e17348c6ce8
thomas-fulton <thomas@fultondesigns.co.uk>
2022-02-22 22:19:21 +0000

    comment out parse_args(): parser obj always created for sphinxcontrib.autoprogram

--- cardgames/__main__.py
+++ cardgames/__main__.py
@@ -4,38 +4,39 @@ from cardgames.games import snap_game
 import argparse
 
 
-def parse_args():
-    """
-    Parses command line arguments for :py:func:`main()`
-    :return: args
-    :rtype: :py:class:`argparse.Namespace`
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-g', '--game', type=str, choices=['snap', 'poker'], help='Specify a game, either to start '
-                                                                                  'up and play, or to read the game\'s '
-                                                                                  'instructions')
-    parser.add_argument('-l', '--loadout', type=str, default='new game', help='Resume a saved game (Not currently '
-                                                                              'available)')
-    parser.add_argument('-p', '--player', type=str, dest='players', action='append', help='Add a single player. (At '
-                                                                                          'least two players with '
-                                                                                          'unique names are required '
-                                                                                          'for a game.)')
-    # ='append' adds each occurrence of player to list of players
-    parser.add_argument('-i', '--instructions', action='store_true', help='Print instructions for a specified '
-                                                                          '<game>.')
-    args = parser.parse_args()
-    if args.instructions is True and args.game is None:
-        parser.error('<game> required with --instructions: please specify for which game you would like\
-         to display instructions.')
-    # for arg in vars(args):
-    #    print("{} is {}".format(arg, getattr(args, arg)))
-    return args
+#def parse_args():
+#    """
+#    Parses command line arguments for :py:func:`main()`
+#    :return: args
+#    :rtype: :py:class:`argparse.Namespace`
+#    """
+parser = argparse.ArgumentParser()
+parser.add_argument('-g', '--game', type=str, choices=['snap', 'poker'], help='Specify a game, either to start '
+                                                                              'up and play, or to read the game\'s '
+                                                                              'instructions')
+parser.add_argument('-l', '--loadout', type=str, default='new game', help='Resume a saved game (Not currently '
+                                                                          'available)')
+parser.add_argument('-p', '--player', type=str, dest='players', action='append', help='Add a single player. (At '
+                                                                                      'least two players with '
+                                                                                      'unique names are required '
+                                                                                      'for a game.)')
+# ='append' adds each occurrence of player to list of players
+parser.add_argument('-i', '--instructions', action='store_true', help='Print instructions for a specified '
+                                                                      '<game>.')
+args = parser.parse_args()
+if args.instructions is True and args.game is None:
+    parser.error('<game> required with --instructions: please specify for which game you would like\
+    to display instructions.')
+
+  # for arg in vars(args):
+  #    print("{} is {}".format(arg, getattr(args, arg)))
+#    return args
 
 
 def main():
     """Launches game module using parsed command line arguments.
     """
-    args = parse_args()
+    args = parser.parse_args()
     print("\n\n\n        Welcome to Python cardgames!\n\n\n")
 
     try:
