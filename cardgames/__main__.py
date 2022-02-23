58e648126dd406e04c1f61a0cb1161b3011544cd
thomas-fulton <thomas@fultondesigns.co.uk>
2022-02-23 19:18:36 +0000

    parsing improvements

--- cardgames/__main__.py
+++ cardgames/__main__.py
@@ -1,42 +1,49 @@
 #!/usr/bin/env python
+import argparse
 from cardgames.games import poker_game
 from cardgames.games import snap_game
-import argparse
 
 
-#def parse_args():
-#    """
-#    Parses command line arguments for :py:func:`main()`
-#    :return: args
-#    :rtype: :py:class:`argparse.Namespace`
-#    """
-parser = argparse.ArgumentParser()
-parser.add_argument('-g', '--game', type=str, choices=['snap', 'poker'], help='Specify a game, either to start '
-                                                                              'up and play, or to read the game\'s '
-                                                                              'instructions')
-parser.add_argument('-l', '--loadout', type=str, default='new game', help='Resume a saved game (Not currently '
-                                                                          'available)')
-parser.add_argument('-p', '--player', type=str, dest='players', action='append', help='Add a single player. (At '
-                                                                                      'least two players with '
-                                                                                      'unique names are required '
-                                                                                      'for a game.)')
-# ='append' adds each occurrence of player to list of players
-parser.add_argument('-i', '--instructions', action='store_true', help='Print instructions for a specified '
-                                                                      '<game>.')
-args = parser.parse_args()
-if args.instructions is True and args.game is None:
-    parser.error('<game> required with --instructions: please specify for which game you would like\
-    to display instructions.')
 
-  # for arg in vars(args):
-  #    print("{} is {}".format(arg, getattr(args, arg)))
-#    return args
+def create_parser():
+    """
+    Parses command line arguments for :py:func:`main()`
+    :return: args
+    :rtype: :py:class:`argparse.Namespace`
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--game', type=str, choices=['snap', 'poker'], help='Specify a game, either to start '
+                                                                                  'up and play, or to read the game\'s '
+                                                                                  'instructions')
+    parser.add_argument('-l', '--loadout', type=str, default='new game', help='Resume a saved game (Not currently '
+                                                                              'available)')
+    parser.add_argument('-p', '--player', type=str, dest='players', action='append', help='Add a single player. (At '
+                                                                                          'least two players with '
+                                                                                          'unique names are required '
+                                                                                          'for a game.)')
+    # ='append' adds each occurrence of player to list of players
+    parser.add_argument('-i', '--instructions', action='store_true', help='Print instructions for a specified '
+                                                                          '<game>.')
+    #args = parser.parse_args()
+    #if args.instructions is True and args.game is None:
+    #    parser.error('<game> required with --instructions: please specify for which game you would like\
+    #    to display instructions.')
+
+    
+    # for arg in vars(args):
+    #    print("{} is {}".format(arg, getattr(args, arg)))
+    return parser
 
 
 def main():
     """Launches game module using parsed command line arguments.
     """
-    args = parser.parse_args()
+    args = create_parser().parse_args()
+    if args.instructions is True and args.game is None:
+        args.error('<game> required with --instructions: please specify for which game you would like\
+        to display instructions.')
+
+
     print("\n\n\n        Welcome to Python cardgames!\n\n\n")
 
     try:
