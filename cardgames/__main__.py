10fbfa2914ddb3cce25b3020c0662cbf47c25145
thomas-fulton <thomas@fultondesigns.co.uk>
2022-11-08 15:49:55 +0000

    resolve conflict?

--- cardgames/__main__.py
+++ cardgames/__main__.py
@@ -4,15 +4,18 @@ import time
 from cardgames.games import poker_game
 from cardgames.games import snap_game
 
+
 def create_parser():
     """Parses command line arguments for :py:func:`main()`
     :return: args
     :rtype: :py:class:`argparse.Namespace`
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-g', '--game', type=str, choices=['snap', 'poker'], help='Specify a game, either to start '
-                                                                                  'up and play, or to read the game\'s '
-                                                                                  'instructions')
+    parser.add_argument('-g', '--game', type=str, choices=['snap', 'poker'], default=None, help='Specify a game, '
+                                                                                                'either to start '
+                                                                                                'up and play, or to '
+                                                                                                'read the game\'s '
+                                                                                                'instructions')
     parser.add_argument('-l', '--loadout', type=str, default='new game', help='Resume a saved game (Not currently '
                                                                               'available)')
     parser.add_argument('-p', '--player', type=str, dest='players', action='append', help='Add a single player. (At '
@@ -20,8 +23,8 @@ def create_parser():
                                                                                           'unique names are required '
                                                                                           'for a game.)')
     # ='append' adds each occurrence of player to list of players
-    parser.add_argument('-i', '--instructions', action='store_true', help='Print instructions for a specified '
-                                                                          '<game>.')
+    parser.add_argument('-i', '--instructions', action='store_true', help='Print instructions for a specified game'
+                                                                          '( see --game <game>).')
     args = parser.parse_args()
     if args.instructions is True and args.game is None:
         parser.error('<game> required with --instructions: please specify for which game you would like to display '
@@ -58,6 +61,7 @@ def main():
 
 if __name__ == '__main__':
     import sys
+
     sys.exit(main())
 
 # TODO bot automation
